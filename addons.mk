5fe7d8fa5924566869b1c37ab453b073a571573a
mydongistiny <jaysonedson@gmail.com>
2015-01-09 13:16:50 -0800

    Update SuperSu, added selinux to permissive, added v4a and added a hosts file.

--- addons.mk
+++ addons.mk
@@ -6,7 +6,7 @@ PRODUCT_COPY_FILES += \
     vendor/xd/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
 
 PRODUCT_PROPERTY_OVERRIDES += \
-  ro.supersu.version=2.43
+  ro.supersu.version=2.44
 endif
 
 # Chromium Prebuilt
@@ -14,11 +14,35 @@ ifeq ($(PRODUCT_PREBUILT_WEBVIEWCHROMIUM),yes)
 -include prebuilts/chromium/$(TARGET_DEVICE)/chromium_prebuilt.mk
 endif
 
+# Build Updater
 PRODUCT_PACKAGES += \
     OpenDelta
 
+# My Overrides
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.delta.version=$(CM_VERSION)
+    ro.delta.version=$(CM_VERSION) \
+    ro.build.selinux=0
 
+# My Product Copies 
 PRODUCT_COPY_FILES += \
-    vendor/xd/prebuilt/common/bin/mpdecision:system/bin/mpdecision
+    vendor/xd/prebuilt/common/bin/mpdecision:system/bin/mpdecision \
+    vendor/xd/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
+    vendor/xd/prebuilt/common/etc/init.d/96setenforce:system/etc/init.d/96setenforce
+
+#Viper4Android
+PRODUCT_COPY_FILES += \
+    vendor/xd/prebuilt/common/priv-app/V4A/V4A.apk:system/priv-app/V4A/V4A.apk \
+    vendor/xd/prebuilt/common/lib/soundfx/libv4a_fx_ics.so:system/lib/soundfx/libv4a_fx_ics.so \
+    vendor/xd/prebuilt/common/etc/audio_effects.conf:system/etc/audio_effects.conf \
+    vendor/xd/prebuilt/common/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    vendor/xd/prebuilt/common/addon.d/91-v4a.sh:system/addon.d/91-v4a.sh
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    lpa.decode=false \
+    lpa.releaselock=false \
+    lpa.use-stagefright=false \
+    tunnel.decode=false
+
+#Hosts
+PRODUCT_COPY_FILES += \
+    vendor/xd/prebuilt/common/etc/hosts:system/etc/hosts
